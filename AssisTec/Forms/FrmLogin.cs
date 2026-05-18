d5b2d2b198f2286958fa3b9983c99bcecdf974e6
CarlosEZaul <carlosezzddomingos@gmail.com>
2026-05-18 10:06:06 -0300

    fix: Fechar a conexaõ na funcao VerificarGerentePadrão() no FrmLogin.cs

--- AssisTec/Forms/FrmLogin.cs
+++ AssisTec/Forms/FrmLogin.cs
@@ -146,6 +146,7 @@ namespace AssisTec
                         
                     }
                 }
+                con.CloseConnection();
             }
             catch (Exception e)
             {
