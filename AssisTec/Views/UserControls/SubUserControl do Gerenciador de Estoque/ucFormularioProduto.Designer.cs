47824c2ad9653f456a9fd1ad30a6bd59f29e143b
CarlosEZaul <carlosezzddomingos@gmail.com>
2026-07-03 12:08:44 -0300

    feat: desing do FORMULÁRIO DE PRODUTOS

--- AssisTec/Views/UserControls/SubUserControl do Gerenciador de Estoque/ucFormularioProduto.Designer.cs
+++ AssisTec/Views/UserControls/SubUserControl do Gerenciador de Estoque/ucFormularioProduto.Designer.cs
@@ -4,12 +4,12 @@ namespace AssisTec.UserControls.SubUserControl_do_Gerenciador_de_Estoque
 {
     partial class ucFormularioProduto
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -23,7 +23,7 @@ namespace AssisTec.UserControls.SubUserControl_do_Gerenciador_de_Estoque
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
         /// <summary>
         /// Required method for Designer support - do not modify
@@ -31,11 +31,262 @@ namespace AssisTec.UserControls.SubUserControl_do_Gerenciador_de_Estoque
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label33 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.cbUnidade = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.mtbPrecoCompra = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mtbPrecoVenda = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtQuantidadeMinima = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label33
+            // 
+            this.label33.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label33.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label33.Location = new System.Drawing.Point(96, 25);
+            this.label33.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label33.Name = "label33";
+            this.label33.Size = new System.Drawing.Size(348, 18);
+            this.label33.TabIndex = 222;
+            this.label33.Text = "FORMULÁRIO DE PRODUTOS";
+            this.label33.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(44, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 224;
+            this.label2.Text = "Descrição:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.BackColor = System.Drawing.Color.White;
+            this.txtDescricao.Location = new System.Drawing.Point(44, 77);
+            this.txtDescricao.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(460, 20);
+            this.txtDescricao.TabIndex = 223;
+            // 
+            // cbUnidade
+            // 
+            this.cbUnidade.FormattingEnabled = true;
+            this.cbUnidade.Location = new System.Drawing.Point(44, 134);
+            this.cbUnidade.Name = "cbUnidade";
+            this.cbUnidade.Size = new System.Drawing.Size(460, 21);
+            this.cbUnidade.TabIndex = 242;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label9.Location = new System.Drawing.Point(44, 110);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 18);
+            this.label9.TabIndex = 241;
+            this.label9.Text = "Unidade";
+            // 
+            // mtbPrecoCompra
+            // 
+            this.mtbPrecoCompra.Location = new System.Drawing.Point(44, 302);
+            this.mtbPrecoCompra.Mask = "0.00";
+            this.mtbPrecoCompra.Name = "mtbPrecoCompra";
+            this.mtbPrecoCompra.Size = new System.Drawing.Size(460, 20);
+            this.mtbPrecoCompra.TabIndex = 244;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(44, 281);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 18);
+            this.label1.TabIndex = 243;
+            this.label1.Text = "Preço de compra:";
+            // 
+            // mtbPrecoVenda
+            // 
+            this.mtbPrecoVenda.Location = new System.Drawing.Point(44, 358);
+            this.mtbPrecoVenda.Mask = "0.00";
+            this.mtbPrecoVenda.Name = "mtbPrecoVenda";
+            this.mtbPrecoVenda.Size = new System.Drawing.Size(460, 20);
+            this.mtbPrecoVenda.TabIndex = 246;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(44, 337);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 18);
+            this.label3.TabIndex = 245;
+            this.label3.Text = "Preço de Venda:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(44, 170);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 18);
+            this.label4.TabIndex = 248;
+            this.label4.Text = "Quantidade";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.BackColor = System.Drawing.Color.White;
+            this.txtQuantidade.Location = new System.Drawing.Point(44, 188);
+            this.txtQuantidade.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(460, 20);
+            this.txtQuantidade.TabIndex = 247;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 9.75F);
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(44, 223);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 18);
+            this.label5.TabIndex = 250;
+            this.label5.Text = "Quantidade Mínima:";
+            // 
+            // txtQuantidadeMinima
+            // 
+            this.txtQuantidadeMinima.BackColor = System.Drawing.Color.White;
+            this.txtQuantidadeMinima.Location = new System.Drawing.Point(44, 241);
+            this.txtQuantidadeMinima.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtQuantidadeMinima.Name = "txtQuantidadeMinima";
+            this.txtQuantidadeMinima.Size = new System.Drawing.Size(460, 20);
+            this.txtQuantidadeMinima.TabIndex = 249;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.btnFechar);
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.btnLimpar);
+            this.panel2.Location = new System.Drawing.Point(127, 492);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(279, 61);
+            this.panel2.TabIndex = 251;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnFechar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnFechar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnFechar.Location = new System.Drawing.Point(178, 14);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(79, 33);
+            this.btnFechar.TabIndex = 102;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnSave.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSave.Location = new System.Drawing.Point(10, 14);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(78, 33);
+            this.btnSave.TabIndex = 100;
+            this.btnSave.Text = "Salvar";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnLimpar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnLimpar.Location = new System.Drawing.Point(94, 14);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(78, 33);
+            this.btnLimpar.TabIndex = 101;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            // 
+            // ucFormularioProduto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ucFormularioProduto";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(55)))), ((int)(((byte)(76)))));
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtQuantidadeMinima);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.mtbPrecoVenda);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.mtbPrecoCompra);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbUnidade);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label33);
+            this.Name = "ucFormularioProduto";
+            this.Size = new System.Drawing.Size(543, 569);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLimpar;
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.TextBox txtQuantidadeMinima;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.MaskedTextBox mtbPrecoCompra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox mtbPrecoVenda;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.ComboBox cbUnidade;
+        private System.Windows.Forms.Label label9;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescricao;
+
+        private System.Windows.Forms.Label label33;
 
         #endregion
     }
--- AssisTec/Views/UserControls/SubUserControl do Gerenciador de Estoque/ucFormularioProduto.cs
+++ AssisTec/Views/UserControls/SubUserControl do Gerenciador de Estoque/ucFormularioProduto.cs
@@ -2,7 +2,7 @@
 
 namespace AssisTec.UserControls.SubUserControl_do_Gerenciador_de_Estoque
 {
-    public partial class ucFormularioProduto : Form
+    public partial class ucFormularioProduto : UserControl
     {
         public ucFormularioProduto()
         {
